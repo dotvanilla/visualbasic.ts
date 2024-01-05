712c9b6f842d7c6ea63fd5edea9d71a4a1b9459a
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-01-05 18:37:12 +0800

    Update Scanner.vb

--- src/Roslyn/Scanner.vb
+++ src/Roslyn/Scanner.vb
@@ -1,3 +1,16 @@
+Imports Microsoft.VisualBasic.ApplicationServices.Development
+Imports Microsoft.VisualBasic.ApplicationServices.Development.VisualStudio.vbproj
+Imports vbproj = Microsoft.VisualBasic.ApplicationServices.Development.VisualStudio.vbproj.Xml.Project
+
 Public Class Scanner
+
+    ReadOnly vbproj As vbproj
+    ReadOnly assembly As AssemblyInfo
+
+    Sub New(vbproj As vbproj)
+        Me.vbproj = vbproj
+        Me.assembly = vbproj.AssemblyInfo
+    End Sub
+
 
 End Class
