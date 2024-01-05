42e93ec6535b78f71ef36bf7dd9495c21b51ec0b
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-01-05 22:40:17 +0800

    needs a general code module

--- src/Roslyn/Scanner.vb
+++ src/Roslyn/Scanner.vb
@@ -1,3 +1,5 @@
+Imports Microsoft.CodeAnalysis.VisualBasic
+Imports Microsoft.CodeAnalysis.VisualBasic.Syntax
 Imports Microsoft.VisualBasic.ApplicationServices.Development
 Imports Microsoft.VisualBasic.ApplicationServices.Development.VisualStudio.vbproj
 Imports vbproj = Microsoft.VisualBasic.ApplicationServices.Development.VisualStudio.vbproj.Xml.Project
@@ -7,10 +9,33 @@ Public Class Scanner
     ReadOnly vbproj As vbproj
     ReadOnly assembly As AssemblyInfo
 
+    Public ReadOnly Property Workspace As Workspace
+
     Sub New(vbproj As vbproj)
         Me.vbproj = vbproj
         Me.assembly = vbproj.AssemblyInfo
+        Me.Workspace = New Workspace(vbproj.RootNamespace)
     End Sub
 
+    ''' <summary>
+    ''' scan all vb source files of current vbproj
+    ''' </summary>
+    Public Function ScanVBFiles() As Workspace
+        For Each file As String In vbproj.EnumerateSourceFiles(skipAssmInfo:=True, fullName:=True)
+            Call ScanVBFile(file)
+        Next
+
+        Return Workspace
+    End Function
+
+    Private Sub ScanVBFile(filepath As String)
+        Dim syntax As CompilationUnitSyntax = VisualBasicSyntaxTree _
+           .ParseText(filepath.SolveStream) _
+           .GetRoot
+
+        For Each obj As StatementSyntax In syntax.Members
+
+        Next
+    End Sub
 
 End Class
---
+++ src/Roslyn/Workspace.vb
@@ -0,0 +1,13 @@
+﻿Public Class Workspace
+
+    Public ReadOnly Property [Namespace] As String
+
+    Sub New(ns As String)
+        [Namespace] = ns
+    End Sub
+
+    Public Overrides Function ToString() As String
+        Return [Namespace]
+    End Function
+
+End Class
